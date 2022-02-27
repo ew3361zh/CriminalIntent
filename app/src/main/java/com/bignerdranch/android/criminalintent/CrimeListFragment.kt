25e320af22a8b06812e2c8ce810f099611f3e5f4
ndyt <ntomlinson@gmail.com>
2022-02-27 16:07:25 -0600

    Added recycler adapter/viewholder/fragment, made some style changes, chapters 9-10 in the book

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,127 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment : Fragment() {
+
+    private lateinit var crimeRecyclerView: RecyclerView
+    private var adapter: CrimeAdapter? = null
+
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProvider(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    // allows CrimeListFragment to use the fragment_crime_list layout view and find the RecylcerView in the layout file
+    // when create recycler view, immediately give it a layoutmanager object for it to be able to work
+    // recycler view does not position items on screen. that is delegated to layoutmanager which
+    // also defines how scrolling works
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        updateUI()
+
+        return view
+    }
+
+    // connect adapter to recycler view
+    private fun updateUI() {
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+    }
+
+    // implement a ViewHolder - recyclerView expects an item view to be wrapped in an instance of a ViewHolder
+    // which stores a ref to an item's view
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
+    View.OnClickListener {
+
+        private lateinit var crime: Crime
+
+        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+        // when given a Crime to bind, CrimeHolder will now update the title and date TextViews to reflect
+        // the state of the Crime
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = this.crime.title
+            dateTextView.text = this.crime.date.toString()
+            solvedImageView.visibility = if(crime.isSolved) {
+                View.VISIBLE
+                } else {
+                    View.GONE
+                }
+            }
+
+        override fun onClick(v: View) {
+            Toast.makeText(context, "${crime.title} pressed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // create second inner class for adapter which is a controller object that sites between the RecyclerView
+    // and data set which RecyclerView should display
+    // Adapter is responsible for creating necessary ViewHolders and binding ViewHolders to data from model layer
+    private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+
+        // responsible for creating a view to display and wrapping view in a view holder and returning the result
+        // here, inflate list_item_view and pass resulting view to new instance of CrimeHolder
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(view)
+        }
+
+        // responsible for populating a fiven holder with crime from given position. use the data to set
+        // the text in the corresponding text views
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+//            holder.apply {
+//                titleTextView.text = crime.title
+//                dateTextView.text = crime.date.toString()
+//            }
+            holder.bind(crime)
+        }
+
+        // returns number of items in the list of crimes for recyclerview to know how many items
+        // are in data set backing it
+        override fun getItemCount(): Int {
+            return crimes.size // in book example, there is no return Int and it just says crimes.size
+        }
+
+    }
+
+    // good to supply newInstance function that activities can call to get instance of a fragment
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+}
